0e7387acfc3a32741b3dc4993586eeee99820ceb
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-08-02 16:22:25 +0300

    updated protfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContactMeComponent,AboutMeComponent,SkillsComponent],
+  imports: [RouterOutlet,ContactMeComponent,AboutMeComponent,SkillsComponent,PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
