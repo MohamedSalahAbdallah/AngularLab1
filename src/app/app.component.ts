bc6743f9c95c069ce08ab9b030e1a3b520474c24
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-08-02 15:31:59 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ContactMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
